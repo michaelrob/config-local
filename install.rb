9ed74fcb7a1fe971cbb5cd674d265a4664b6536b
Michael Robinson <seraph890@gmail.com>
2017-01-06 12:08:35 +0900

    updated comments + added git ignore global

--- install.rb
+++ install.rb
@@ -8,24 +8,26 @@ if File.directory? "#{root}/.git"
     # toDo: required updates
 	# ask for username
 	# cp files instead of linking them
-    # Setup grails settings
+
+    # setup grails settings
     dot_grails_dir = "#{ENV['HOME']}/.grails"
     FileUtils.mkdir dot_grails_dir unless File.directory? dot_grails_dir
 
     grails_settings_config = "#{dot_grails_dir}/settings.groovy"
     FileUtils.touch grails_settings_config
 
-    # setup
+    # setup scripts
     scripts_home = "#{ENV['HOME']}/scripts"
     FileUtils.touch scripts_home
 
-    # symlink config files
-    FileUtils.cp_r 'scripts/.bash_profile', '~/.bash_profile'
-    FileUtils.cp_r 'scripts/.bash_rc', '~/.bash_rc'
-    FileUtils.cp_r 'scripts/.gitconfig', '~/.gitconfig'
-    FileUtils.cp_r 'scripts/.vimrc', '~/.vimrc'
+    # copy config files
+    FileUtils.cp 'scripts/.bash_profile', '~/.bash_profile'
+    FileUtils.cp 'scripts/.bash_rc', '~/.bash_rc'
+    FileUtils.cp 'scripts/.gitconfig', '~/.gitconfig'
+    FileUtils.cp 'scripts/git/gitignore', '~/.gitignore_global'
+    FileUtils.cp 'scripts/.vimrc', '~/.vimrc'
 
-    # symlink completion scripts
+    # copy completion scripts
     FileUtils.cp_r 'scripts/completion_scripts/grails_completion', '~/scripts/grails_completion'
     FileUtils.cp_r 'scripts/completion_scripts/rake_completion', '~/scripts/rake_completion'
     FileUtils.cp_r 'scripts/completion_scripts/ssh_completion', '~/scripts/ssh_completion'
