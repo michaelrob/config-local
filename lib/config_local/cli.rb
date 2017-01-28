399e66e030a50460536b059a366b41664197b32a
Michael Robinson <seraph890@gmail.com>
2017-01-28 12:39:12 +0900

    added list functionality

--- lib/config_local/cli.rb
+++ lib/config_local/cli.rb
@@ -1,15 +1,27 @@
 require "thor"
+require 'json'
 
 module ConfigLocal
   class Veur < Thor
     desc "install PACKAGE", "This will install the selected package"
     def install(package)
-      puts "installing...."
     end
 
     desc "list available packages", "This will list packages available for installation"
     def list
-      puts "Available packages:"
+      # ToDo: better formatting, we should limit the amount of characters for easy
+        # reading, and also right justify the install advice
+      Dir.foreach('packages') do |item|
+        next if item == '.' or item == '..' or File.exists?("packages/#{item}/information.json") == false
+        data = JSON.parse(File.read("packages/#{item}/information.json"))
+        puts "--------------------------------------------------------------------------------"
+        puts data["name"]
+        puts ""
+        puts data["description"]
+        puts ""
+        puts "configlocal install #{data['name']}"
+        puts ""
+      end
     end
   end
 end
