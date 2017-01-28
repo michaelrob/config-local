ea4d5bacc278c45cd11179289820fc1f09b204f7
Michael Robinson <seraph890@gmail.com>
2017-01-28 11:03:23 +0900

    added some basic puts, this really needs to be fleshed out more

--- lib/config_local/cli.rb
+++ lib/config_local/cli.rb
@@ -4,12 +4,12 @@ module ConfigLocal
   class Veur < Thor
     desc "install PACKAGE", "This will install the selected package"
     def install(package)
-
+      puts "installing...."
     end
 
     desc "list available packages", "This will list packages available for installation"
     def list
-      
+      puts "Available packages:"
     end
   end
 end
