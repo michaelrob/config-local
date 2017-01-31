f80782c4329730ab6fb59bd04c14d2c890467ddc
Michael Robinson <seraph890@gmail.com>
2017-01-31 17:35:46 +0900

    added a bash install class

---
+++ packages/bash/bash_install.rb
@@ -0,0 +1,12 @@
+require 'fileutils.rb'
+
+class BashInstall
+
+  # ToDo: add things related to bash installing
+
+  # ToDo: install bash_config
+    # install bashrc
+    # install completion fileu
+
+  # ToDo: change this to do some bash_config generation rather than copying my bash_config.
+end
